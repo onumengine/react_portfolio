cf79a15c2252c4bc7f797b5f4c8702d3a1150069
onumengine <onumdev@gmail.com>
2021-02-13 21:24:14 +0100

    moved the tabbar to the molecules folder

---
+++ src/views/molecules/TabBar.js
@@ -0,0 +1,11 @@
+import '../../App.css';
+
+const TabBar = () => 
+    <nav>
+        <button>Home</button>
+        <button>About me</button>
+        <button>Hire me</button>
+        <button>Contact me</button>
+    </nav>
+
+export default TabBar
