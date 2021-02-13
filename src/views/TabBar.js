a906f4ad66c6f915250b31e79745adec611f79e6
onumengine <onumdev@gmail.com>
2021-02-13 20:37:34 +0100

    Added the tab bar

---
+++ src/views/TabBar.js
@@ -0,0 +1,13 @@
+import App from "../App";
+
+import '../App.css';
+
+const TabBar = () => 
+    <nav>
+        <button>Home</button>
+        <button>About me</button>
+        <button>Hire me</button>
+        <button>Contact me</button>
+    </nav>
+
+export default TabBar
