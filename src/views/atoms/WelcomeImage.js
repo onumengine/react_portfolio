73580823153f27a8647c1c4910f75374c939b5f2
onumengine <onumdev@gmail.com>
2021-02-13 21:29:51 +0100

    gave the welcome image a block display

--- src/views/atoms/WelcomeImage.js
+++ src/views/atoms/WelcomeImage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import '../../images/undraw_1.png'
 
 const WelcomeImage = () => 
-    <img alt="Supposed to have the welcome image here" src="undraw_1.png" width="200px"></img>
+    <div>
+        <img alt="Supposed to have the welcome image here" src="undraw_1.png" width="200px"></img>
+    </div>
+    
 
 export default WelcomeImage
