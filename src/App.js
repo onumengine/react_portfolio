0e0296a6f1d964f56b9898a1c2ed7a1c76f25a68
onumengine <onumdev@gmail.com>
2021-02-13 21:27:51 +0100

    added the welcome image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import TabBar from './views/TabBar';
+import WelcomeImage from './views/atoms/WelcomeImage';
+import TabBar from './views/molecules/TabBar.js';
+import WelcomeBanner from './views/molecules/WelcomeBanner';
+import './images/undraw_1.png'
 
 function App() {
   return (
     <div className="App">
       <TabBar/>
+      <WelcomeBanner/>
+      <WelcomeImage/>
     </div>
   );
 }
---
+++ src/views/atoms/WelcomeImage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../../images/undraw_1.png'
+
+const WelcomeImage = () => 
+    <img alt="Supposed to have the welcome image here" src="undraw_1.png" width="200px"></img>
+
+export default WelcomeImage
