6d308742fd0b5d0afe2eca960132a9450d292019
onumengine <onumdev@gmail.com>
2021-02-14 20:42:49 +0100

    added the contact and site credits sections to the footer section

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import WelcomeImage from './views/atoms/WelcomeImage';
 import TabBar from './views/molecules/TabBar.js';
 import WelcomeBanner from './views/molecules/WelcomeBanner';
-import './images/undraw_1.png'
 import MainArticle from './views/molecules/MainArticle';
 import ProjectsHeading from './views/molecules/ProjectsHeading';
+import ProjectItem from './views/organisms/ProjectItem';
+import FooterSection from './views/organisms/FooterSection';
 
 function App() {
   return (
@@ -13,9 +14,19 @@ function App() {
       <WelcomeBanner/>
       <WelcomeImage/>
       <MainArticle/>
-      <ProjectsHeading/>
+      <ProjectsHeading />
+      <ProjectItem/>
+      <ProjectItem/>
+      <ProjectItem/>
+      <FooterSection/>
     </div>
   );
+}
+
+var descriptionObject = {
+  "name": "KumManie's store",
+  "description": "Online store dealing in perfumes of all scents, delivering nationwide",
+  "buildTools": "HTML/CSS/JS and Django"
 }
 
 export default App;
--- src/views/organisms/FooterSection.js
+++ src/views/organisms/FooterSection.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import ContactSection from '../molecules/ContactSection'
 import SiteCredits from '../molecules/SiteCredits'
 
 const FooterSection = () =>
     <footer>
+        <ContactSection/>
         <SiteCredits/>
     </footer>
 
