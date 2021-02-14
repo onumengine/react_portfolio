7271cf369b8e1275e69f14eb219f50e94d3286a3
onumengine <onumdev@gmail.com>
2021-02-14 20:41:58 +0100

    added the contact section

---
+++ src/views/molecules/ContactSection.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactSection = () => 
+    <div>
+        <h1>Let's Build Together</h1>
+        <p>I'm in the market for a full-time position as a frontend/fulllstack developer</p>
+        <br></br>
+        <p>Your awesome project is just a couple interesting conversations away</p>
+        <button>Send an Email</button>
+    </div>
+
+export default ContactSection
