700ead6de97a7595767b91c5fbcbe8d3defe2336
onumengine <onumdev@gmail.com>
2021-02-15 17:09:24 +0100

    styled the tab bar

--- src/views/molecules/TabBar.js
+++ src/views/molecules/TabBar.js
@@ -2,10 +2,10 @@ import '../../App.css';
 
 const TabBar = () => 
     <nav>
-        <button>Home</button>
-        <button>About me</button>
-        <button>Hire me</button>
-        <button>Contact me</button>
+        <button className="text-button">Home</button>
+        <button className="text-button">About me</button>
+        <button className="text-button">Hire me</button>
+        <button className="flat-button">Contact me</button>
     </nav>
 
 export default TabBar
