74b64e019a6ce4cd5a126bda2a220d1e028c2897
onumengine <onumdev@gmail.com>
2021-02-15 16:25:15 +0100

    styled the footer

--- src/views/molecules/ContactSection.js
+++ src/views/molecules/ContactSection.js
@@ -1,12 +1,11 @@
 import React from "react"
 
 const ContactSection = () => 
-    <div>
+    <div id="contact-div">
         <h1>Let's Build Together</h1>
-        <p>I'm in the market for a full-time position as a frontend/fulllstack developer</p>
-        <br></br>
+        <p>I'm in the market for a full-time position as a frontend/fullstack developer</p>
         <p>Your awesome project is just a couple interesting conversations away</p>
-        <button>Send an Email</button>
+        <button className="flat-button" id="send-email-button">Send an Email</button>
     </div>
 
 export default ContactSection
--- src/views/molecules/SiteCredits.js
+++ src/views/molecules/SiteCredits.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 const SiteCredits = () =>
-    <p>Designed by Isaac Sokari, built by me</p>
+    <p id="site-credits">Designed by Isaac Sokari, built by me</p>
 
 export default SiteCredits
