4c557da9d2098a48d2388ea3f4b517234318851a
onumengine <onumdev@gmail.com>
2021-02-15 17:30:45 +0100

    padded the main article

--- src/views/molecules/MainArticle.js
+++ src/views/molecules/MainArticle.js
@@ -3,15 +3,17 @@ import React from 'react'
 const MainArticle = () =>
     <article>
         <h2>Skills and Portfolio</h2>
-        <p>
-            Over the years, constantly buildinng new things has given me the opportunity
-            to pick up amazing skills I use to build beautiful software. They include:
-        </p>
-        <br></br>
-        <p>Some of the skills I've picked up in web development include:</p>
-        <p>Frontend:  HTML/CSS/JS SCSS Bootstrap EJS templates React</p>
-        <p>Backend: NodeJS(Express) Django Firebase</p>
-        <p>Collaboration: Git and Github Heroku Netlify</p>
+        <div>
+            <p>
+                Over the years, constantly building new things has given me the opportunity
+                to pick up amazing skills I use to build beautiful software. They include:
+            </p>
+            <br></br>
+            <p>Some of the skills I've picked up in web development include:</p>
+            <p>Frontend:  HTML/CSS/JS SCSS Bootstrap EJS templates React</p>
+            <p>Backend: NodeJS(Express) Django Firebase</p>
+            <p>Collaboration: Git and Github Heroku Netlify</p>
+        </div>
     </article>
 
 
